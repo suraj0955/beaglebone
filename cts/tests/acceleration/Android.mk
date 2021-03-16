fdd83072f309ec3d5d59f285409d8cc40ddcf098
suraj0955 <73685137+suraj0955@users.noreply.github.com>
2021-03-16 12:04:42 +0000

    Update Android.mk

--- cts/tests/acceleration/Android.mk
+++ cts/tests/acceleration/Android.mk
@@ -30,6 +30,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CtsAccelerationTestStubs
 
-LOCAL_SDK_VERSION := system_current
+LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
